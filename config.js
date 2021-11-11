9f6807899bba084b08eefe8c6775b75d974f5309
InforTech07 <Techpilot07@outlook.com>
2021-11-11 16:04:03 -0600

    update config

--- config.js
+++ config.js
@@ -1,4 +1,29 @@
 require('dotenv').config();
+
+module.exports = {
+    api:{
+        port: process.env.PORT || 8080,
+    },
+    jwt: {
+        secret: process.env.JWT_SECRET || 'notasecret!',
+    },
+    sqlserver:{
+        dbUser:'adminsa',
+        dbPassword:'Db_2021_Agua_S',
+        dbServer:'dbdiraguasaneamiento.ctyjderc9fzr.us-east-2.rds.amazonaws.com',
+        dbDatabase:'dbmuestras',
+    },
+    cloudinary:{
+        cloud:'https://api.cloudinary.com/v1_1/municipalidad-san-jose-chacaya/image/upload',
+        presets:'gg5gskkt'
+    }
+}
+
+
+
+
+
+/*
 module.exports = {
     api:{
         port: process.env.PORT || 8080,
@@ -16,4 +41,4 @@ module.exports = {
         cloud:process.env.CLOUDINARY_URL,
         presets:process.env.PRESETS
     }
-}
+}*/

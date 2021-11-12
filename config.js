af1a02879119afee2c2eb9886be85b7eccf76f54
InforTech07 <Techpilot07@outlook.com>
2021-11-11 19:10:33 -0600

    setting config

--- config.js
+++ config.js
@@ -8,6 +8,29 @@ module.exports = {
         secret: process.env.JWT_SECRET || 'notasecret!',
     },
     sqlserver:{
+        dbUser:process.env.DB_USUARIO,
+        dbPassword:process.env.DB_PASSWORD,
+        dbServer:process.env.DB_SERVER,
+        dbDatabase:process.env.DB_DATABASE,
+    },
+    cloudinary:{
+        cloud:process.env.CLOUDINARY_URL,
+        presets:process.env.PRESETS
+    }
+}
+
+
+
+
+/*
+module.exports = {
+    api:{
+        port: process.env.PORT || 8080,
+    },
+    jwt: {
+        secret: process.env.JWT_SECRET || 'notasecret!',
+    },
+    sqlserver:{
         dbUser:'adminsa',
         dbPassword:'Db_2021_Agua_S',
         dbServer:'dbdiraguasaneamiento.ctyjderc9fzr.us-east-2.rds.amazonaws.com',
@@ -18,7 +41,7 @@ module.exports = {
         presets:'gg5gskkt'
     }
 }
-
+*/
 
 
 
